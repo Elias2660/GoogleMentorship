0956ba9ebc43a777f659befba6303be76b892c3a
Elias Xu <flam0799@gmail.com>
2024-04-10 11:11:00 -0400

    finish draft of scripts

--- data/final.py
+++ data/final.py
@@ -3,6 +3,8 @@ from openai import OpenAI
 import tiktoken
 import dotenv
 import asyncio
+import pandas as pd
+import numpy as np
 from pyppeteer import launch
 
 # ? getting the api keys
@@ -11,7 +13,7 @@ GOOGLE_API = dotenv.get_key("../.env", "GOOGLE_SEARCH_API")
 GPT_API = dotenv.get_key("../.env", "GPT_API")
 SEARCH_ENGINE_ID = dotenv.get_key("../.env", "SEARCH_ENGINE_ID")
 
-NUMBER = 5
+NUMBER = 5 # for testing purposes :3
 
 model = "gpt-4-turbo-preview"
 context = 128000
@@ -23,8 +25,8 @@ if __name__ == "__main__":
     if NUMBER is -1, then run for all elements in the dataset
     else if then run the thing for the number of viruses that exist
     """
-    print("hello world")
-
+    data = pd.DataFrame(columns =["Virus Name", "R0 Value", "Range of R0 Values", "Source1" "Source 2"])
+    
 # the data gathering steps
 
 
@@ -69,7 +71,12 @@ def get_number_of_tokens(text: str, model="gpt-4") -> list:
 # the summarize the textual data, saving as many numbers of as possible
 
 
-def summarize(text: str, token_limit = context) -> str:
+def summarize(
+    text: str,
+    token_limit=context,
+    goal="finding r0 values in relation to diseases",
+    model=model,
+) -> str:
     """
     Given a lot of text, summarize it
 
@@ -80,20 +87,76 @@ def summarize(text: str, token_limit = context) -> str:
         Important information includes specific numbers and numerals.
     """
 
-    command = """"""
-    tokens = get_number_of_tokens(text + command + system_message)
-    while (tokens > token_limit):
-        # cut the tokens down to half
-        ...
-    
-    client = OpenAI(api_key=GPT_API)
+    command = f"""
+        Please summarize the following text into 7 or less sentences, paying special attention to the {goal}.
+        Keep numbers related to {goal} accurate, and them within the context that was in the webpage.
+        
+        Here's the text to summarize:
+        {text}
+    """
+    tokens = get_number_of_tokens(command + system_message)
+    if tokens > token_limit:
+        # summarize both sides of the text
+        return (
+            summarize(text[0 : int(text.length) - 1])
+            + "\n"
+            + summarize(text[int(text.length - 1)])
+        )
 
-    return ""
+    client = OpenAI(api_key=GPT_API)
+    completion = client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": system_message},
+            {"role": "user", "content": command},
+        ],
+    )
+
+    return completion.choices[0].message.content
 
     # use the extracted data to plug into the gpt
 
 
-def getResult(summarized_data: str) -> str: ...
-
-
-# %%
+def get_result(summarized_data: str, virus: str) -> np.ndarray:
+    """
+    given the summarized context, return a string containing the result
+    """
+    client = OpenAI(api_key=GPT_API)
+    system_message = f"""
+    Write as an output no more words than specified. 
+    
+    In total, there should be at most four lines out.
+    
+    The first one should have two numbers, separated with a comma, the next should have a citation, and the next should either be blank or have a citation, as a URL.
+    
+    If you provide an url providing information on the r naught value of the virus {virus}, please make sure it is a reliable source and included the r0 value of {virus} in the text.
+    
+    Here's some information: 
+    {summarized_data}
+    """
+    command = f"""
+    Write as an output no more words than specified. In total, there should be at most four lines out. The first one should have two numbers, separated with a comma, the next should have a citation, and the next should either be blank or have a citation, preferably as a URL.
+    
+    Write the average R0 value of {virus} in the first line. Do it in numeric form to the 2nd decimal. If the R0 values do not exist, estimate one value from the range given and put the range after that value with a comma separating them. This should be one line. If you cannot find a range, print “NaN”
+    
+    In the following two lines, put the sources, preferably as URLs, that helped lead you to this conclusion, after that end the response. If you only have one source, please put it on the first line, leaving the second one blank.
+    
+    Double-check your result. In the following line, output either “true” if both the citations relate to the average R0 value of the virus {virus}. If not, output “false”. 
+    
+    Write as an output no more words than specified. In total, there should be at most four lines out. The first one should have two numbers separated with a comma, the next should have a some form of citation, and the next should either be blank or have some form of citation. The final should have a true/false value based upon if you found the sources to be actually related when double checking your work.
+    """
+    completion = client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": system_message},
+            {"role": "user", "content": command},
+        ],
+    )
+    array_return = np.full(shape=5, fill_value=np.nan)
+    elements = np.split("\n")
+    array_return[0] = virus
+    array_return[1], array_return[2] = elements[0].split(",")
+    array_return[3] = elements[1]
+    array_return[4] = elements[2] if elements[2] == "" else np.nan
+    
+    return array_return
